ad82822e66a3c4be6e6564cb79c6a6394fe8187c
Arina1004 <41124539+Arina1004@users.noreply.github.com>
2018-09-15 20:52:14 +0300

    Update generate_password.rb

--- generate_password.rb
+++ generate_password.rb
@@ -1,44 +1,47 @@
 module PasswordGenerator
 
-  def initialize(length, num=0)
-    #password length
-    @length_pass=length
-    #number of digits in the password
-    @num=num
-  end
-  
-  def generate_vowels() 
-     vowels = ["a","e","i","y","o","u"]
-     letter = vowels[rand(vowels.length)]
-     return letter
-  end
+def initialize(length, num = 0)
+  # Password length
+  @length_pass = length
+  # Number of digits in the password
+  @num = num
+end  
 
-  def generate_consonants() 
-     consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","x","w","z"]
-     letter = consonants[rand(consonants.length)]
-     return letter
-  end
-
-  def generate_number()
-    number = rand(10)
-    return number
-  end
-
-  def generate()
-    password=""
-    remain = ((@length_pass-@num)%5)
-    if (remain!=0)
-      remain = 5 - remain
-    end
-    len = ((@length_pass-@num)+remain)/5
-    for i in 1..len
-      password+=generate_consonants().capitalize+generate_vowels() + generate_consonants()+generate_vowels() + generate_consonants()
-    end
-    password=password[0...-remain]
-    for i in 1..@num
-      password+=generate_number().to_s
-    end
-    return password
-  end
- 
+def password
+  generate
 end
+
+private
+ 
+def generate_vowels
+  vowels = ['a' , 'e' , 'i' , 'y' , 'o' , 'u']
+  vowels[rand(vowels.length)]
+end
+
+def generate_consonants
+  consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm',
+   'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'w', 'z']
+  consonants[rand(consonants.length)]
+end
+
+def generate_number
+  rand(10)
+end
+
+def generate
+  password = ""
+  remain = ((@length_pass-@num)%5)
+  remain = remain == 0 ?
+  remain : 5 - remain
+  len = ((@length_pass - @num) + remain)/5
+    for i in 1..len
+    password += generate_consonants.capitalize + generate_vowels + 
+    generate_consonants + generate_vowels + generate_consonants
+  end
+  password = password[0...-remain]
+  for i in 1..@num
+    password += generate_number.to_s
+  end
+  return password
+end
+
